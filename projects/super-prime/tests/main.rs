86f1d86c09f359eee6c1a69937de245f4a5eb1aa
Aster <aster@vers.site>
2023-05-17 20:30:29 +0800

    🚀 Search for 100-digits super prime

--- projects/super-prime/tests/main.rs
+++ projects/super-prime/tests/main.rs
@@ -1,11 +1,9 @@
-use std::str::FromStr;
-use super_prime::{BigUint, insert_digit, super_prime};
-
+use std::{fs::File, io::Write, path::Path, str::FromStr};
+use super_prime::{insert_digit, super_prime, BigUint};
 #[test]
 fn ready() {
     println!("it works!")
 }
-
 
 #[test]
 fn test2() {
@@ -22,9 +20,28 @@ fn test2() {
 }
 
 #[test]
-fn test() {
+fn test() -> std::io::Result<()> {
+    const LIMIT: usize = 100;
+    let here = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests").canonicalize()?;
+    let mut prime2 = File::create(here.join("prime2.py"))?;
+    let mut prime3 = File::create(here.join("prime3.py"))?;
+    let mut prime5 = File::create(here.join("prime5.py"))?;
+    let mut prime7 = File::create(here.join("prime7.py"))?;
     let start = BigUint::from(2usize);
-    for n in super_prime(&start, 10).into_iter().rev() {
-        println!("{}", n);
+    for n in super_prime(&start, LIMIT).into_iter() {
+        writeln!(prime2, "{}", n.to_string())?;
     }
+    let start = BigUint::from(3usize);
+    for n in super_prime(&start, LIMIT).into_iter() {
+        writeln!(prime3, "{}", n.to_string())?;
+    }
+    let start = BigUint::from(5usize);
+    for n in super_prime(&start, LIMIT).into_iter() {
+        writeln!(prime5, "{}", n.to_string())?;
+    }
+    let start = BigUint::from(7usize);
+    for n in super_prime(&start, LIMIT).into_iter() {
+        writeln!(prime7, "{}", n.to_string())?;
+    }
+    Ok(())
 }
